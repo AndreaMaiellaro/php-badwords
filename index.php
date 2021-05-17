9542f882a582a513667971aac87a88d5a1acd9a7
shrekt444 <andrea.maie444@gmail.com>
2021-05-17 15:15:52 +0200

    fine

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+    $parola = $_GET['censurare'];
+    echo $parola;
+
+    $mia_stringa = "Mi piace il pesce e a volte ci gratto sopra il formaggio";
+
+    $lunghezza = strlen($mia_stringa);
+
+    $risultato = str_replace('formaggio', '***', $mia_stringa);
+
+?>
+
+<p><?php echo $lunghezza; ?></p>
+
+<h1><?php echo $risultato; ?></h1>
