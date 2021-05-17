2688eb65cdf16ea5d79d97686d9018a078ca2a22
shrekt444 <andrea.maie444@gmail.com>
2021-05-17 16:57:05 +0200

    fine corretto

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php 
     $parola = $_GET['censurare'];
-    echo $parola;
+    // echo $parola;
 
     $mia_stringa = "Mi piace il pesce e a volte ci gratto sopra il formaggio";
 
     $lunghezza = strlen($mia_stringa);
 
-    $risultato = str_replace('formaggio', '***', $mia_stringa);
+    $risultato = str_replace($parola, '***', $mia_stringa);
 
 ?>
 
